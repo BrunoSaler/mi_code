7d7704fb9cc25febd2a60e4f43db5efdb1b7622a
BrunoSaler <bruno.salerno97@gmail.com>
2023-11-16 13:05:23 -0300

    add after 3

---
+++ Coderhouse/after 3/tablero_2d.py
@@ -0,0 +1,22 @@
+class Tablero2d:
+
+
+    def __init__(self, filas, columnas, entrada):
+        self.filas = filas
+        self.columnas = columnas
+        self.entrada = entrada
+
+
+    def imprimir(self):
+        una_fila = "_ " * self.columnas
+
+        for i in range(0, self.filas):
+            print(una_fila)
+
+
+
+mi_tablero = Tablero2d(15, 4, [1,1])
+
+print("_" * 90)
+mi_tablero.imprimir()
+print("_" * 90)
