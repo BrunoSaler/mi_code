73cbac1b7a9cbf51ae2a0e58f753d4cc42de2a9a
BrunoSaler <bruno.salerno97@gmail.com>
2023-11-23 19:51:32 -0300

    clase 19 a

--- Coderhouse/ejercicios complementarios/actividades_haciaPF/actividad_3_haciaPF/actividad_3/act3/views.py
+++ Coderhouse/ejercicios complementarios/actividades_haciaPF/actividad_3_haciaPF/actividad_3/act3/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from datetime import date
 
+def date_format(date):                                              #le da formato a las fechas
+    months = ("Enero", "Febrero", "Marzo", "Abri", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+    day = date.day
+    month = months[date.month - 1]                                  #como la tupla arranca del elemento 0, le debo de restar 1
+    year = date.year
+    return f"{day} de {month} de {year}"
+
 def view_bienvenida(request):
     return render(request, "act3/welcome.html")#aca el template es estático
 
 def view_autor(request):
     nombre = "Bruno"
     apellido = "Salerno"
-    fecha = date.today()
+    fecha = date_format(date.today())
     diccionario = {
         'nombre': nombre,
         'apellido': apellido,
