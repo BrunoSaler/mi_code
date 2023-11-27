94d6236d0ccca75d151efdf3f85671cba9d7d575
BrunoSaler <bruno.salerno97@gmail.com>
2023-11-27 01:54:16 -0300

    creado superuser y models

--- Tercera_pre-entrega_Salerno/preentrega3/app1/admin.py
+++ Tercera_pre-entrega_Salerno/preentrega3/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.usuario)
+admin.site.register(models.producto)
+admin.site.register(models.compras)
--- Tercera_pre-entrega_Salerno/preentrega3/app1/models.py
+++ Tercera_pre-entrega_Salerno/preentrega3/app1/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    nacimiento = models.DateField()
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class producto(models.Model):
+    producto = models.CharField(max_length=100)
+    disponible = models.IntegerField()
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100, unique=True)
+    precio = models.DecimalField(decimal_places=2, max_digits=9)
+
+    def __str__(self):
+        return self.modelo
+
+class compras(models.Model):
+    usuario = models.ForeignKey(usuario, null=True, on_delete=models.SET_NULL)
+    fecha_compra = models.DateField()
+    producto = models.ForeignKey(producto, on_delete=models.PROTECT)
+
+
+
+
+
+
+
+
+
