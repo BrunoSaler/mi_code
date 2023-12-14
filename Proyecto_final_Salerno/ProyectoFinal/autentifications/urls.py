958a0c6ac1e0485b0838e5ba2d61afea81d68ad0
BrunoSaler <bruno.salerno97@gmail.com>
2023-12-13 21:34:42 -0300

    implementado login y logout PF

--- Proyecto_final_Salerno/ProyectoFinal/autentifications/urls.py
+++ Proyecto_final_Salerno/ProyectoFinal/autentifications/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from autentifications.views import *
 
 urlpatterns = [
     path("signup/", view_register),
+    path("login/", view_login),
+    path("logout/", view_logout),
 ]
--- Proyecto_final_Salerno/ProyectoFinal/autentifications/views.py
+++ Proyecto_final_Salerno/ProyectoFinal/autentifications/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from . import forms
 
 def view_register(request):
@@ -20,3 +20,28 @@ def view_register(request):
         else:
             messages.error(request, "Intentelo nuevamente en unos minutos.")
             return render(request,"autentifications/registro.html",{"form": form})
+
+def view_login(request):
+
+    if request.user.is_authenticated:
+        messages.error(request, f"{usuario} ya esta logueado.")
+        return render(request,"base/home.html")
+    if request.method == "GET":
+        return render(
+            request,"autentifications/login.html",{"form": AuthenticationForm()})
+    else:
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            usuario = data["username"]
+            password = data["password"]
+            modelo = authenticate(username=usuario, password=password)
+            login(request, modelo)
+            return render(request,"base/home.html")
+        else:
+            messages.error(request, "Intentelo nuevamente en unos minutos.")
+            return render(request,"autentifications/login.html",{"form": formulario})
+        
+def view_logout(request):
+    logout(request)
+    return render(request,"autentifications/logout.html")
